325a574c0d4cb6b70d72fbd77f7b48597b849e89
Pritesh Durgapal <priteshdurgapal@Priteshs-MacBook-Air.local>
2021-09-10 23:22:43 +0530

    First commit

---
+++ src/main/java/com/prit/ultimatems/UltimatemsApplication.java
@@ -0,0 +1,13 @@
+package com.prit.ultimatems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltimatemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltimatemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prit/ultimatems/UltimatemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prit.ultimatems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltimatemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
